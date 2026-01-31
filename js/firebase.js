f6c404a8b769f34580347eff4b00a81e2363370b
Anurag69og <anuragsoni9669@protonmail.com>
2026-02-01 00:10:58 +0530

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,3 +1,7 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+
 const firebaseConfig = {
   apiKey: "AIzaSyCqH0qto__icf60ja-IZLyNIaJNMeF1wkY",
   authDomain: "study-challenge-96dab.firebaseapp.com",
@@ -6,3 +10,7 @@ const firebaseConfig = {
   messagingSenderId: "493026362192",
   appId: "1:493026362192:web:c059d9f9a46475afa6a255"
 };
+
+export const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
